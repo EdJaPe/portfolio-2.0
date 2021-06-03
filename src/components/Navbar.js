02eac26375d5eef15dc95ab8924807118f96748c
edJaPe <edv06jara@gmail.com>
2021-06-02 22:43:47 -0700

    fied navbar responsivenes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../logo2.0.png";
 
 const Navbar = () => {
+
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
                 <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"></img></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
